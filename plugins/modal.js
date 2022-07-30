1b2d50b8255b16471f47c60714e199a5d3659ba5
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2022-07-30 21:23:31 +0400

    feat: add open and close methods in CONSOLE

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,1 +1,39 @@
-$.modal = function () { }
+
+function _createModal(options) {
+    const modal = document.createElement('div')
+    modal.classList.add('modalWrapper')
+    modal.insertAdjacentHTML('afterbegin', `
+        <div class="modalBackground">
+            <div class="modalWindow">
+                <div class="modalHeader">
+                    <span class="modalTitle">Modal title</span>
+                    <span class="modalClose">&times;</span>
+                </div>
+                <div class="modalContent">
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p>Lorem ipsum dolor sit.</p>
+                </div>
+                <div class="modalFooter">
+                    <button>OK</button>
+                    <button>Cancel</button>
+                </div>
+            </div>
+        </div>
+    `)
+    document.body.appendChild(modal)
+    return modal
+}
+
+$.modal = function (options) {
+    const $modal = _createModal(options)
+
+    return {
+        open() {
+            $modal.classList.add('open')
+        },
+        close() {
+            $modal.classList.remove('open')
+        },
+        destroy() { }
+    }
+}
