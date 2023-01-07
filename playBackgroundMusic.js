6e787235b7e4fd7c59e47bd57a024542756eae0c
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-08 01:07:52 +0400

    feat: add Joyful and Mystical bg music buttons

--- playBackgroundMusic.js
+++ playBackgroundMusic.js
@@ -1,5 +1,37 @@
+let backgroundMusicDiv = document.querySelector('.backgroundMusicDiv')
+
 let JoyfulMusic = document.querySelector('.Joyful')
 let MysticalMusic = document.querySelector('.Mystical')
 
 let JoyfulMusicSound = new Audio('./What-Do-We-Do.mp3')
 let MysticalMusicSound = new Audio('./My-Jolly-Sailor-Bold.mp3')
+
+let joyfulBackgroundMusic = true
+let mysticalBackgroundMusic = true
+
+
+JoyfulMusic.addEventListener('click', () => {
+    if (joyfulBackgroundMusic) {
+        backgroundMusicDiv.innerText = 'Joyful Music is playing'
+        JoyfulMusicSound.volume = 0.25
+        JoyfulMusicSound.play()
+        joyfulBackgroundMusic = false
+    } else {
+        backgroundMusicDiv.innerText = 'Music is not playing'
+        JoyfulMusicSound.pause()
+        joyfulBackgroundMusic = true
+    }
+})
+
+MysticalMusic.addEventListener('click', () => {
+    if (mysticalBackgroundMusic) {
+        backgroundMusicDiv.innerText = 'Mystical Music is playing'
+        MysticalMusicSound.volume = 0.25
+        MysticalMusicSound.play()
+        mysticalBackgroundMusic = false
+    } else {
+        backgroundMusicDiv.innerText = 'Music is not playing'
+        MysticalMusicSound.pause()
+        mysticalBackgroundMusic = true
+    }
+})
