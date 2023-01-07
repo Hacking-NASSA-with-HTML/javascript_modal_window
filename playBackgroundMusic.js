140c259538fe2dcb4191293bf7d97f164b5d5e2b
Hacking-NASSA-with-HTML <follow.trend7777@gmail.com>
2023-01-08 00:52:25 +0400

    feat: add correct path to mp3

--- playBackgroundMusic.js
+++ playBackgroundMusic.js
@@ -1,3 +1,5 @@
 let JoyfulMusic = document.querySelector('.Joyful')
 let MysticalMusic = document.querySelector('.Mystical')
 
+let JoyfulMusicSound = new Audio('./What-Do-We-Do.mp3')
+let MysticalMusicSound = new Audio('./My-Jolly-Sailor-Bold.mp3')
